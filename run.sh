e548ae98b4d3767d87feecd5ac4c1707df6ff4c4
Michael <iamwuming@163.com>
2025-03-07 21:20:32 +0800

    Optimize qemu run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-~/github/qemu/build/qemu-system-aarch64 -M virt -cpu cortex-a57 -nographic -kernel test64.elf
+~/github/qemu/build/qemu-system-aarch64 \
+    -nographic \
+    -M virt \
+    -cpu cortex-a57 \
+    -kernel test64.elf
 
